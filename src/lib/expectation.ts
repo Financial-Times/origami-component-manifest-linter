50f53758d86b324cb26383b73ad8aed0175d2d71
chee <chee@snoot.club>
2020-09-29 15:34:11 +0100

    add expectation codedocs

--- src/lib/expectation.ts
+++ src/lib/expectation.ts
@@ -1,13 +1,5 @@
 import {JsonValueU, JsonValue} from "./json-value"
 
-export type TypeOfType = "string" | "boolean" | "array" | "number" | "object"
-
-export interface TypeOf {
-	type: "typeOf"
-	expected: TypeOfType[]
-	received: JsonValueU
-}
-
 /**
  * a generic expectation for printing special messages.
  *
@@ -18,12 +10,29 @@ export interface Message {
 	type: "message"
 }
 
+/**
+ * Create a {@link Message} expectation
+ */
 export function message(): Message {
 	return {
 		type: "message",
 	}
 }
 
+export type TypeOfType = "string" | "boolean" | "array" | "number" | "object"
+
+/**
+ * an expectation for unexpected types
+ */
+export interface TypeOf {
+	type: "typeOf"
+	expected: TypeOfType[]
+	received: JsonValueU
+}
+
+/**
+ * Create a {@link TypeOf} expectation
+ */
 export function typeOf(expected: TypeOfType[], received: JsonValueU): TypeOf {
 	return {
 		type: "typeOf",
@@ -32,11 +41,17 @@ export function typeOf(expected: TypeOfType[], received: JsonValueU): TypeOf {
 	}
 }
 
+/**
+ * an expectation for things that were meant to be files
+ */
 export interface File {
 	type: "file"
 	received: JsonValueU
 }
 
+/**
+ * Create a {@link File} expectation
+ */
 export function file(received: JsonValueU): File {
 	return {
 		type: "file",
@@ -44,11 +59,17 @@ export function file(received: JsonValueU): File {
 	}
 }
 
+/**
+ * an expectation for things that were meant to be Urls
+ */
 export interface Url {
 	type: "url"
 	received: JsonValueU
 }
 
+/**
+ * Create a {@link Url} expectation
+ */
 export function url(received: JsonValueU): Url {
 	return {
 		type: "url",
@@ -56,12 +77,18 @@ export function url(received: JsonValueU): Url {
 	}
 }
 
+/**
+ * an expectation for a specific value
+ */
 export interface ValueOf<T> {
 	type: "valueOf"
 	expected: T
 	received: JsonValueU
 }
 
+/**
+ * Create a {@link ValueOf} expectation
+ */
 export function valueOf<T>(expected: T, received: JsonValueU): ValueOf<T> {
 	return {
 		type: "valueOf",
@@ -70,12 +97,18 @@ export function valueOf<T>(expected: T, received: JsonValueU): ValueOf<T> {
 	}
 }
 
+/**
+ * an expectation for a value that's one of a set
+ */
 export interface MemberOf<T extends JsonValue> {
 	type: "memberOf"
 	list: T[]
 	received: JsonValueU
 }
 
+/**
+ * Create a {@link MemberOf} expectation
+ */
 export function memberOf<T extends JsonValue>(
 	list: T[],
 	received: JsonValueU
@@ -87,12 +120,18 @@ export function memberOf<T extends JsonValue>(
 	}
 }
 
+/**
+ * an expectation for a strings that begin a certain way
+ */
 export interface StartsWith {
 	type: "startsWith"
 	prefix: string
 	received: JsonValueU
 }
 
+/**
+ * Create a {@link StartsWith} expectation
+ */
 export function startsWith(
 	expectedPrefix: string,
 	received: JsonValueU
@@ -104,12 +143,18 @@ export function startsWith(
 	}
 }
 
+/**
+ * an expectation for a strings that match a regexp
+ */
 export interface Match {
 	type: "match"
 	regexp: RegExp
 	received: JsonValueU
 }
 
+/**
+ * Create a {@link Match} expectation
+ */
 export function match(regexp: RegExp, received: JsonValueU): Match {
 	return {
 		type: "match",
@@ -118,12 +163,18 @@ export function match(regexp: RegExp, received: JsonValueU): Match {
 	}
 }
 
+/**
+ * an expectation for a strings that are at least a certain length
+ */
 export interface LongerThan {
 	type: "longerThan"
 	minimum: number
 	received: JsonValueU
 }
 
+/**
+ * Create a {@link LongerThan} expectation
+ */
 export function longerThan(minimum: number, received: JsonValueU): LongerThan {
 	return {
 		type: "longerThan",
@@ -132,6 +183,9 @@ export function longerThan(minimum: number, received: JsonValueU): LongerThan {
 	}
 }
 
+/**
+ * an expectation for a strings that are at most a certain length
+ */
 export interface ShorterThan {
 	type: "shorterThan"
 	maximum: number
